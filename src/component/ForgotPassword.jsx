e7219f7709657e7a2997dd22f4ece8b774eabb4f
mueez sanwo <mueezsanwo@gmail.com>
2022-08-28 11:23:33 +0100

    anewcommit

--- src/component/ForgotPassword.jsx
+++ src/component/ForgotPassword.jsx
@@ -1,4 +1,4 @@
-import React  from "react";
+import React from "react";
 import PhoneInput from "react-phone-number-input";
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom'
@@ -10,104 +10,80 @@ import logo from '../images/logo.png';
 // import { Helmet } from "react-helmet";
 
 const ForgotPassword = () => {
-   const [display, setDisplay] = useState('');
-   const [value, setValue] = useState();
+    const [displays, setDisplay] = useState('');
+    const [value, setValue] = useState();
 
-   const navigate = useNavigate();
-   function Next() {
-     navigate('/Help')
-   }
+    const navigate = useNavigate();
+    function Next() {
+        navigate('/Help')
+    }
 
-  function handleEmailDisplay(){
-      setDisplay(
-      <div>
-          <p>We will send you an email with instructions on how to reset your password.</p>
-          <input type='email' id='emailText'  placeholder='name@example.com' required/><br /> 
-          <button className="btn">Email Me</button>
-      </div>
-      )
-  }
+    function handleEmailDisplay() {
+        setDisplay(
+            <div className="fdiv">
+                <p className="ptexts">We will send you an email with instructions on how to reset your password.</p>
+                <input type='email' id='emailTexts' placeholder='name@example.com' required /><br />
+                <button className="btns">Email Me</button>
+            </div>
+        )
+    }
 
-  function handleTextDisplay(){
-      setDisplay(
-      <div>
-          <p>We will text you a verification code to reset your password. Message and data rates may apply.</p>     
-          <div className="PhoneInput">
-            <PhoneInput 
-             placeholder=""
-             value={value}
-             onChange={setValue}
-             id='input'
-             defaultCountry='NG'
-             required
-            
-            />
-            </div> 
-          <button className="btn">Text Me</button>
-      </div>
-      )
-  }
+    function handleTextDisplay() {
+        setDisplay(
+            <div className="fdiv">
+                <p className="ptexts">We will text you a verification code to reset your password. Message and data rates may apply.</p>
+                <div className="PhoneInput">
+                    <PhoneInput
+                        placeholder=""
+                        value={value}
+                        onChange={setValue}
+                        id='inputfone'
+                        defaultCountry='NG'
+                        required
 
-  useEffect(() => {
-    handleEmailDisplay()
-  }, [])
+                    />
+                </div>
+                <button className="btns">Text Me</button>
+            </div>
+        )
+    }
+
+    useEffect(() => {
+        handleEmailDisplay()
+    }, [])
 
 
-return (
-  <body> 
-  <div className="fullbody">
-      <div className="header headerbox">
-          <img className="header-item header-item1" src={logo} alt="netflix logo" />
-          <h2 className="header-item header-item2" onClick={() => navigate('SignUp')}>Sign In</h2>
-      </div>
-      <form className="content">
-          <h1 className="top-content">Forgot Email/Password</h1>
-          <p>How would you like to reset your password?</p>
-          <div className="inputs">
-          <input type="radio" onClick={handleEmailDisplay} name="forgotPassword" className="radio"  required/> 
-          <label>Email</label><br></br> 
-          <div></div>
-          </div>
-          <div className="inputs">
-          <input type="radio" onClick={handleTextDisplay} name="forgotPassword" className="radio" required/> 
-          <label>Text Message(SMS)</label>
-          <div></div>
-          </div>
-          <div> {display} </div>
-           <p><Link to='/Help' className="linktext">I don't remember my email or Phone</Link></p>
-      </form>
-        <div className="recaptcha">
-        <p> <span>This page is Protected by Google reCAPTCHA to ensure you're not a bot.</span> <button className="rebtn">Learn more</button></p>
-        </div>
-        <div className='signupFooter footer' style={{height:"270px"}}>
-                <div className="ftr-content ftr-content1">
-                    <div className="contact">
-                        <a>Questions? <a href="tel:000-800-040-1843">Contact us
-                        </a></a>
+    return (
+        <body>
+            <div className="fullbody fdiv">
+                <div className="header headerbox fdiv">
+                    <img className="header-item header-item1" src={logo} alt="netflix logo" />
+                    <h2 className="header-item header-item2" onClick={() => navigate('SignUp')}>Sign In</h2>
+                </div>
+                <form className="content">
+                    <h1 className="top-content">Forgot Email/Password</h1>
+                    <p className="ptexts">How would you like to reset your password?</p>
+                    <div className="inputs fdiv">
+                        <input type="radio" onClick={handleEmailDisplay} name="forgotPassword" className="radio" required />
+                        <label>Email</label><br></br>
+                        <div></div>
                     </div>
-                    <div className="ftr">
-                        <div>
-                            <a onClick={() => navigate("/faq")}>FAQ</a><br /><br />
-                            <a href="/Home">Cookie Prefrences</a>
-                        </div>
-                        <div>
-                            <a onClick={() => navigate("/help")}>Help Center</a><br /><br />
-                            <a href="/Home">Corperate Information</a>
-                        </div>
-                        <div>
-                            <a href="/Home">Terms of Use</a>
-                        </div>
-                        <div>
-                            <a href="/Home">Privacy</a>
-                        </div>
-                        {/* ignore the hrefs for now */}
+                    <div className="inputs fdiv">
+                        <input type="radio" onClick={handleTextDisplay} name="forgotPassword" className="radio" required />
+                        <label>Text Message(SMS)</label>
+                        <div></div>
                     </div>
-                    </div>
-                  </div>
-                </div>    
-       </body>
-       
-)
+                    <div className="fdiv"> { displays }  </div>
+                    <p><Link to='/Help' className="linktext">I don't remember my email or Phone</Link></p>
+                </form>
+                <div className="recaptchatexts">
+                    <h1 className="ptext retexts">This page is Protected by Google reCAPTCHA to ensure you're
+                    not a bot.<a href="#">Learn more</a></h1>
+                </div>
+            </div>
+        </body>
+
+    )
 }
 
 export default ForgotPassword;
--- src/component/PageForm.jsx
+++ src/component/PageForm.jsx
@@ -9,36 +9,36 @@ function PageForm(props) {
     return (
         <>
           <div>
-           <div className="formhead">
-              <img className="formhead-item formhead-item1" src={logo} alt="logo" />
+           <div className="pageformhead">
+              <img className="pageformhead-item pageformhead-item1" src={logo} alt="logo" />
               {/* <h2 className="formhead-item formhead-item2">Sign In</h2> */}
-              <div className="formhead-item formhead-item2">
+              <div className="pageformhead-item pageformhead-item2">
                 <a href="/Login">Sign In</a>
               </div>
            </div>
-           <form className="formbox">
-                <p>Step 1 0f 3</p>
-                <h1 className="formh1">Create a password to start your membership</h1>
-                <h3 className="formh3">Just a few more steps and you're done!
+           <form className="pageformbox1">
+                <p className="pageform3">Step <strong className="pageform3">1</strong> of<strong className="pageform3"> 3</strong></p>
+                <h1 className="pageform1">Create a password to start your membership</h1>
+                <h3 className="pageform2">Just a few more steps and you're done!
                   We hate paperwork, too.
                 </h3>
                 <div>
-                <input type="email" placeholder="Email" className="forminput" required /><br></br>
-                <input type="password" placeholder="Add a Password" className="forminput" required /><br></br>
+                <input type="email" placeholder="Email" className="pageforminputs" required /><br></br>
+                <input type="password" placeholder="Add a Password" className="pageforminputs" required /><br></br>
                 <label className="box">Please do not Email me Netflix special offers
-                <input type="checkbox" />
+                <input type="checkbox" className="pageformcheckbox" />
                 <span className="mark"></span>
                 </label>
                 </div>
                 <button className="formbtn">Next</button>
            </form>
-           <div className=' footer4' style={{height:"250px"}}>
-                <div className="ftr4-box ">
-                    <div className="contact4">
+           <div className='footerpageform1' style={{height:"230px"}}>
+                <div className="footerpageform-box ">
+                    <div className="pageformcontact">
                         <a>Questions? <a href="tel:000-800-040-1843">Contact us
                         </a></a>
                     </div>
-                    <div className="ftr4">
+                    <div className="footerpage4">
                         <div>
                             <a >FAQ</a><br /><br />
                             <a href="#">Cookie Prefrences</a>
